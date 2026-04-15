4375c84f66473edd226b4d269a2e7c3cf4b70f66
Tim Magoun <timmagoun@gmail.com>
2026-04-14 21:05:42 -0700

    fix (#45)

--- host_tests/session_controller_host_test.cc
+++ host_tests/session_controller_host_test.cc
@@ -11,10 +11,10 @@ namespace {
 runtime_config_t ValidConfig(void) {
   runtime_config_t config = {};
 
-  config.port_count = 1;
   config.ports[0].enabled = true;
   config.ports[0].baud_rate = 460800;
-  config.ports[0].timing_mode = PORT_TIMING_DISABLED;
+  config.ports[0].timing_mode = PORT_TIMING_NONE;
+  config.ports[0].sync_edge_mode = SYNC_EDGE_RISING;
 
   return config;
 }
@@ -27,6 +27,8 @@ TEST(SessionControllerTest, BlocksStartWhenConfigInvalid) {
 
   EXPECT_FALSE(session_controller_request_start(&controller, &invalid));
   EXPECT_EQ(session_controller_state(&controller), SESSION_CONFIG_INVALID);
+  EXPECT_EQ(session_controller_last_config_error(&controller),
+            RUNTIME_CONFIG_ERROR_NO_ENABLED_PORTS);
 }
 
 TEST(SessionControllerTest, TransitionsToFaultedOnFatalFault) {
@@ -60,16 +62,30 @@ TEST(SessionControllerTest, EntersReadyOnlyAfterStorageMountAndConfigLoad) {
   EXPECT_EQ(session_controller_state(&controller), SESSION_READY);
 }
 
-TEST(SessionControllerTest, StartsRecordingAutomaticallyOnBootWhenConfigValid) {
+TEST(SessionControllerTest, RejectsConfigLoadAfterRecordingStarts) {
   session_controller_t controller = {};
   runtime_config_t config = ValidConfig();
 
   session_controller_init(&controller);
 
-  ASSERT_EQ(session_controller_mark_storage_ready(&controller), ESP_OK);
-  ASSERT_EQ(session_controller_mark_config_loaded(&controller, &config),
-            ESP_OK);
-  ASSERT_EQ(session_controller_start_autonomously(&controller), ESP_OK);
+  ASSERT_TRUE(session_controller_request_start(&controller, &config));
+  ASSERT_EQ(session_controller_mark_recording(&controller), ESP_OK);
+  EXPECT_EQ(session_controller_mark_config_loaded(&controller, &config),
+            ESP_ERR_INVALID_STATE);
+  EXPECT_EQ(session_controller_mark_storage_ready(&controller),
+            ESP_ERR_INVALID_STATE);
+  EXPECT_EQ(session_controller_state(&controller), SESSION_RECORDING);
+}
+
+TEST(SessionControllerTest, RejectsRepeatStartAfterRecordingStarts) {
+  session_controller_t controller = {};
+  runtime_config_t config = ValidConfig();
+
+  session_controller_init(&controller);
+
+  ASSERT_TRUE(session_controller_request_start(&controller, &config));
+  ASSERT_EQ(session_controller_mark_recording(&controller), ESP_OK);
+  EXPECT_FALSE(session_controller_request_start(&controller, &config));
   EXPECT_EQ(session_controller_state(&controller), SESSION_RECORDING);
 }
 
